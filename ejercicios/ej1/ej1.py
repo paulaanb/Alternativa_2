5095d1dce96841ccc05bb1dbfe6bc29e6c31bc1b
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-12-09 18:09:05 +0100

    funcion resolver

--- ejercicios/ej1/ej1.py
+++ ejercicios/ej1/ej1.py
@@ -3,3 +3,23 @@ import itertools
 import re
 import sys
 
+
+def solve(board):
+  if isinstance(board,list):
+    board = Board(board)
+
+  board.solve()
+
+  if board.invalid: return None
+  if board.solved: return board.board
+
+  empty,empty_i = board.min_empty()
+
+  for candidate in empty.candidates:
+    guess = Board(board)
+    guess.set_cell(empty,candidate,empty_i)
+
+    solution = solve(guess)
+    if solution is not None: return solution
+
+  return None
