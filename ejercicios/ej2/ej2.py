e223cf6fa3b4d34ebfc73d43b0e102b56a060aa1
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-12-09 18:18:06 +0000

    hola

--- ejercicios/ej2/ej2.py
+++ ejercicios/ej2/ej2.py
@@ -2,11 +2,15 @@ def expand(expression):
  # Extraer a, b y n de la expresión
     a, b, n = extract_values_from_expression(expression)
     # Inicializar la cadena resultante como una cadena vacía
-    result = ""
+    result = " "
     # Iterar sobre las potencias de x en el resultado expandido
     for i in range(n, -1, -1):
     # Calcular el coeficiente para el término actual
-    coefficient = n * (n - 1) * ... * (n - i + 1) / i!
+    
+
+        coefficient = n * (n - 1) * ... * (n - i + 1) / i!
+        
+
     # Añadir el término al resultado
-    result += format_term(coefficient, a, i, b)
-return result
+        result += format_term(coefficient, a, i, b)
+    return result
