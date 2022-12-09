65edbbaf1cda5409ec3f2a0cddbcd9565d8cd838
paulaanb <91721496+paulaanb@users.noreply.github.com>
2022-12-09 18:09:53 +0100

    comentarios

--- ejercicios/ej1/ej1.py
+++ ejercicios/ej1/ej1.py
@@ -23,3 +23,10 @@ def solve(board):
     if solution is not None: return solution
 
   return None
+
+# 1 = no candidato; 0 = candidato.
+# El lugar del binario es el número, así que hay 9 lugares.
+#
+# Por ejemplo: 101 010 101
+# 987 654 321
+# Esto significa que [2,4,6,8] son candidatos para esta celda.
